56fa938d0f14953f3e88c45e6d77aa08e26e0c7c
Andrew Chin <achin@eminence32.net>
2015-08-15 17:56:36 -0400

    Add a write method

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,11 @@ use std::convert::AsRef;
 use std::path::Path;
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Write};
 
 use xml::reader::EventReader;
-use xml::reader::events::XmlEvent;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Element {
     /// The name of the Element.  Does not include any namespace info
     pub name: String,
@@ -29,7 +28,7 @@ pub struct Element {
 
 
 fn build<B: Read>(reader: &mut EventReader<B>, mut elem: Element) -> Element {
-    
+    use xml::reader::events::XmlEvent;
     loop {
         match reader.next() {
             XmlEvent::EndElement{ref name} if name.local_name == elem.name => { return elem; }
@@ -49,6 +48,73 @@ fn build<B: Read>(reader: &mut EventReader<B>, mut elem: Element) -> Element {
 }
 
 impl Element {
+
+    /// Parses a file into an Element 
+    ///
+    /// # Panics
+    ///
+    /// Panics on error or other unhandled condition
+    pub fn parse<R: Read>(r: R) -> Element {
+        use xml::reader::events::XmlEvent;
+
+        let mut reader = EventReader::new(r);
+
+
+        loop {
+            match reader.next() {
+                XmlEvent::StartElement{name, attributes, namespace} => {
+                    let mut attr_map = HashMap::new();
+                    for attr in attributes { attr_map.insert(attr.name.local_name, attr.value); }
+
+                    let root = Element{name: name.local_name, attributes: attr_map, children: Vec::new(), text: None};
+                    return build(&mut reader, root);
+                }
+                XmlEvent::EndDocument => break,
+                XmlEvent::Error(e) => panic!("{:?}", e),
+                _ => ()
+
+            }
+        }
+        panic!("Error")
+    }
+
+    fn _write<B: Write>(&self, emitter: &mut xml::writer::EventWriter<B>) {
+        use xml::writer::events::XmlEvent;
+        use xml::name::Name;
+        use xml::namespace::Namespace;
+        use xml::attribute::Attribute;
+
+        let name = Name::local(&self.name);
+        let mut attributes = Vec::with_capacity(self.attributes.len());
+        for (k, v) in self.attributes.iter() {
+            attributes.push(Attribute{name: Name::local(k), value: v});
+        }
+
+        let namespace = Namespace::empty();
+
+
+        emitter.write(XmlEvent::StartElement{name: name, attributes:attributes, namespace: &namespace}).unwrap();
+        if let Some(ref t) = self.text {
+            emitter.write(XmlEvent::Characters(t)).unwrap();
+        }
+        for elem in &self.children {
+            elem._write(emitter);
+        }
+        emitter.write(XmlEvent::EndElement{name: name}).unwrap();
+
+    }
+
+    /// Writes out this element as the root element in an new XML document
+    pub fn write<W: Write>(&self, w:W) {
+        use xml::writer::EventWriter;
+        use xml::writer::events::XmlEvent;
+        use xml::common::XmlVersion;
+
+        let mut emitter = EventWriter::new(w);
+        emitter.write(XmlEvent::StartDocument{version: XmlVersion::Version10, encoding: None, standalone: None}).unwrap();
+        self._write(&mut emitter);
+    }
+
     /// Attempts to find a child element with the given name
     pub fn get_child<K>(&self, k: K) -> Option<&Element> 
       where String: PartialEq<K> {
@@ -62,33 +128,3 @@ impl Element {
 
 }
 
-/// Parses a file into an Element 
-///
-/// # Panics
-///
-/// Panics on error or other unhandled condition
-pub fn parse<P: AsRef<Path>>(p: P) -> Element {
-    let f = File::open(p).unwrap();
-    let mut reader = EventReader::new(f);
-
-
-    loop {
-        match reader.next() {
-            XmlEvent::StartElement{name, attributes, namespace} => {
-                let mut attr_map = HashMap::new();
-                for attr in attributes { attr_map.insert(attr.name.local_name, attr.value); }
-
-                let root = Element{name: name.local_name, attributes: attr_map, children: Vec::new(), text: None};
-                return build(&mut reader, root);
-            }
-            XmlEvent::EndDocument => break,
-            XmlEvent::Error(e) => panic!("{:?}", e),
-            _ => ()
-            
-        }
-    }
-    panic!("Error")
-
-
-
-}
--- tests/suite.rs
+++ tests/suite.rs
@@ -1,35 +1,66 @@
 
 extern crate xmltree;
+extern crate tempfile;
+
 use xmltree::Element;
-use xmltree::parse;
+use std::fs::File;
+use tempfile::TempFile;
 
 
 #[test]
 fn test_01() {
-    let e: Element = parse("tests/data/01.xml");
-    println!("{:#?}", e);
+    let e: Element = Element::parse(File::open("tests/data/01.xml").unwrap());
+    println!("E:=======\n{:#?}", e);
     assert_eq!(e.name, "project");
     let e2: &Element = e.get_child("libraries").expect("Missing libraries child element");
     assert_eq!(e2.name, "libraries");
 
     assert!(e.get_child("doesnotexist").is_none());
 
+    let mut tmp = TempFile::shared(2).unwrap();
+    let t1 = tmp.remove(0);
+    let t2 = tmp.remove(0);
+    e.write(t1);
+
+    let e2 = Element::parse(t2);
+    println!("E2:======\n{:#?}", e2);
+
+    assert_eq!(e, e2);
+
 }
 #[test]
 fn test_02() {
-    let e: Element = parse("tests/data/02.xml");
+    let e: Element = Element::parse(File::open("tests/data/02.xml").unwrap());
     println!("{:#?}", e);
 
 }
 #[test]
 fn test_03() {
-    let e: Element = parse("tests/data/03.xml");
+    let e: Element = Element::parse(File::open("tests/data/03.xml").unwrap());
     println!("{:#?}", e);
 
 }
 #[test]
 fn test_04() {
-    let e: Element = parse("tests/data/04.xml");
+    let e: Element = Element::parse(File::open("tests/data/04.xml").unwrap());
     println!("{:#?}", e);
+
+}
+
+
+#[test]
+fn test_rw() {
+
+    let e: Element = Element::parse(File::open("tests/data/rw.xml").unwrap());
+
+    let mut tmp = TempFile::shared(2).unwrap();
+    let t1 = tmp.remove(0);
+    let t2 = tmp.remove(0);
+    e.write(t1);
+
+    let e2 = Element::parse(t2);
+
+    assert_eq!(e, e2);
+
 
 }
